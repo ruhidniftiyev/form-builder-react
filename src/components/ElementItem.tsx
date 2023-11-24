1eaddd561fbe282a03d395b658c767244d9c36de
ruhidniftiyev <tigerrn22@gmail.com>
2023-11-24 04:21:42 +0400

    finish elements block

--- src/components/ElementItem.tsx
+++ src/components/ElementItem.tsx
@@ -3,14 +3,18 @@ import React, { FC } from 'react';
 type Props = {
   content: string | undefined;
   color: string;
+  title: string;
 };
 
-const ElementItem: FC<Props> = ({ content, color }) => {
+const ElementItem: FC<Props> = ({ content, color, title }) => {
   return (
-    <div
-      className="element-item"
-      style={{ backgroundColor: color, boxShadow: `0px 10px 30px -5px ${color}` }}>
-      <img className="element-item__content-img" src={content} alt="" />
+    <div className="element-item">
+      <div
+        className="element-item__icon"
+        style={{ backgroundColor: color, boxShadow: `0px 10px 30px -5px ${color}` }}>
+        <img className="element-item__icon-img" src={content} alt="" />
+      </div>
+      <p className="element-item__title">{title}</p>
     </div>
   );
 };
--- src/components/ElementList.tsx
+++ src/components/ElementList.tsx
@@ -7,15 +7,27 @@ type Props = {};
 
 const ElementList = (props: Props) => {
   const [elements] = useState<any>([
-    { id: 1, content: text, color: 'orange' },
-    { id: 2, content: number, color: 'green' },
+    { id: 1, content: text, color: 'orange', title: 'Text input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
+    { id: 2, content: number, color: 'green', title: 'Number input' },
   ]);
 
   return (
-    <div className="element-list">
-      {elements.map((el: any) => (
-        <ElementItem {...el} />
-      ))}
+    <div className="elements">
+      <p className="elements__title">ELEMENTS</p>
+      <div className="elements__list">
+        {elements.map((el: any) => (
+          <ElementItem {...el} />
+        ))}
+      </div>
     </div>
   );
 };
