f95828c0f7f3ab6dd6b69bb48cf3b86757d25b49
ruhidniftiyev <tigerrn22@gmail.com>
2023-11-27 22:10:35 +0400

    add select element

--- src/components/FormElements.tsx
+++ src/components/FormElements.tsx
@@ -1,17 +1,35 @@
 import { LuTextCursorInput } from 'react-icons/lu';
+import { MdOutlinePlaylistAddCheck } from 'react-icons/md';
 
 export const TextInput = {
   title: 'input',
   description: 'Allows to write text.',
-    icon: <LuTextCursorInput />,
-  
-    settings: {
+  icon: <LuTextCursorInput />,
 
-    },
+  settings: {},
 
-    render: () => {
-        <>
-            <input type="text" />
-        </>
-    }
+  render: () => {
+    <>
+      <input type="text" />
+    </>;
+  },
+};
+
+export const SelectElement = {
+  title: 'input',
+  description: 'Allows to write text.',
+  icon: <MdOutlinePlaylistAddCheck />,
+
+  settings: {
+    options: [],
+  },
+
+  render: function () {
+    <>
+      <select name="" id="">
+        {this.settings.options.length > 0 &&
+          this.settings.options.map((option: any) => <option value={option}>{option}</option>)}
+      </select>
+    </>;
+  },
 };
