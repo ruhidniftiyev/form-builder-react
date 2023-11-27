2bee9c433a71a990e72eedce603c8be7604fba97
ruhidniftiyev <tigerrn22@gmail.com>
2023-11-28 02:23:26 +0400

    fix bugs

--- src/components/ElementItem.tsx
+++ src/components/ElementItem.tsx
@@ -1,19 +1,18 @@
 import React, { FC } from 'react';
 
-type Props = {
+type ElementItemsProps = {
   icon: string | undefined;
   color: string;
   title: string;
   onClick: () => void;
 };
 
-const ElementItem: FC<Props> = ({ icon, color, title, onClick }) => {
+const ElementItem: FC<ElementItemsProps> = ({ icon, color, title, onClick }) => {
   return (
     <div onClick={onClick} className="element-item">
       <div
         className="element-item__icon"
         style={{ backgroundColor: color, boxShadow: `0px 10px 30px -5px ${color}` }}>
-        {/* <img className="element-item__icon-img" src={icon} alt="" /> */}
         {icon}
       </div>
       <p className="element-item__title">{title}</p>
